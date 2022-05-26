3e2e9e7c7bea238152e84ca9e3fdeb10b8450af8
kho903 <gmldnr2222@naver.com>
2022-05-26 14:08:01 +0900

    feat: add findById, update in PostsService and PostsApiController

--- src/main/java/com/jikim/webservice/springboot/service/posts/PostsService.java
+++ src/main/java/com/jikim/webservice/springboot/service/posts/PostsService.java
@@ -3,8 +3,11 @@ package com.jikim.webservice.springboot.service.posts;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.jikim.webservice.springboot.domain.posts.Posts;
 import com.jikim.webservice.springboot.domain.posts.PostsRepository;
+import com.jikim.webservice.springboot.web.dto.PostsResponseDto;
 import com.jikim.webservice.springboot.web.dto.PostsSaveRequestDto;
+import com.jikim.webservice.springboot.web.dto.PostsUpdateRequestDto;
 
 import lombok.RequiredArgsConstructor;
 
@@ -17,5 +20,25 @@ public class PostsService {
 	@Transactional
 	public Long save(PostsSaveRequestDto requestDto) {
 		return postsRepository.save(requestDto.toEntity()).getId();
+	}
+
+	public PostsResponseDto findById(Long id) {
+		Posts entity = postsRepository.findById(id)
+			.orElseThrow(() -> new IllegalArgumentException(
+				"해당 게시글이 없습니다. id=" + id
+			));
+
+		return new PostsResponseDto(entity);
+	}
+
+	@Transactional
+	public Long update(Long id, PostsUpdateRequestDto requestDto) {
+		Posts posts = postsRepository.findById(id)
+			.orElseThrow(() -> new IllegalArgumentException(
+				"해당 게시글이 없습니다. id=" + id
+			));
+
+		posts.update(requestDto.getTitle(), requestDto.getContent());
+		return id;
 	}
 }
