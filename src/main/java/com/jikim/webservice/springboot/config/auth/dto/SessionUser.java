f947162a80833c3f3c5969a764820f7915db0d29
kho903 <gmldnr2222@naver.com>
2022-05-27 17:47:17 +0900

    feat: add spring-session-jdbc for SessionUser

--- src/main/java/com/jikim/webservice/springboot/config/auth/dto/SessionUser.java
+++ src/main/java/com/jikim/webservice/springboot/config/auth/dto/SessionUser.java
@@ -1,11 +1,13 @@
 package com.jikim.webservice.springboot.config.auth.dto;
 
+import java.io.Serializable;
+
 import com.jikim.webservice.springboot.domain.user.User;
 
 import lombok.Getter;
 
 @Getter
-public class SessionUser {
+public class SessionUser implements Serializable {
 	private String name;
 	private String email;
 	private String picture;
