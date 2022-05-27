bb1bbd82fe13e9bf1624b5ddc541b1da90c186c9
kho903 <gmldnr2222@naver.com>
2022-05-27 17:38:22 +0900

    refactor: add Login User annotation

--- src/main/java/com/jikim/webservice/springboot/web/IndexController.java
+++ src/main/java/com/jikim/webservice/springboot/web/IndexController.java
@@ -1,12 +1,11 @@
 package com.jikim.webservice.springboot.web;
 
-import javax.servlet.http.HttpSession;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import com.jikim.webservice.springboot.config.auth.LoginUser;
 import com.jikim.webservice.springboot.config.auth.dto.SessionUser;
 import com.jikim.webservice.springboot.service.posts.PostsService;
 import com.jikim.webservice.springboot.web.dto.PostsResponseDto;
@@ -18,12 +17,10 @@ import lombok.RequiredArgsConstructor;
 public class IndexController {
 
 	private final PostsService postsService;
-	private final HttpSession httpSession;
 
 	@GetMapping("/")
-	public String index(Model model) {
+	public String index(Model model, @LoginUser SessionUser user) {
 		model.addAttribute("posts", postsService.findAllDesc());
-		SessionUser user = (SessionUser) httpSession.getAttribute("user");
 		if (user != null) {
 			model.addAttribute("userName", user.getName());
 		}
